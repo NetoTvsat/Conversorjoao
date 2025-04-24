40d2d2e0e3d08377c95a29a149726b2748cd551b
NetoTvsat <netotvsat@gmail.com>
2025-04-23 21:59:07 -0300

    Add files via upload

---
+++ Conversorjoao/Conversor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversorjoao
+{
+    internal class Conversor
+    {
+        public double resultado;
+
+        public double ConvertDolar(double reais, double CotacaoDolar)
+        {
+            resultado = reais / CotacaoDolar;
+            return resultado;
+        }
+        public double ConvertPeruano(double reais, double CotacaoPeruano)
+        {
+            resultado = reais * CotacaoPeruano;
+            return resultado;
+        }
+    }
+}
---
+++ Conversorjoao/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversorjoao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorReal, valorDolar, valorPeruano;
+
+            Conversor conversor = new Conversor();
+
+            Console.WriteLine("Informe um valor em reais:");
+            valorReal = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a cotação atual do dólar:");
+            valorDolar = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a cotação atual do soles Peruano:");
+            valorPeruano = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Dólar:{conversor.ConvertDolar(valorReal,valorDolar)}");
+            Console.WriteLine($"Soles peruano:{conversor.ConvertPeruano(valorReal, valorPeruano)}");
+        }
+    }
+}
